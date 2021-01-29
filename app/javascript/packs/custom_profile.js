da109a88e69b7c8afe9643b77e464354fbef7ed8
masao <>
2021-01-29 14:38:20 +0900

    プロフィールの編集でアバター画像をjQueryで表示させる

---
+++ app/javascript/packs/custom_profile.js
@@ -0,0 +1,31 @@
+import $ from 'jquery'
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('#avatar_preview').on('click', () => {
+    $('#avatar_file_field').trigger("click");
+  });
+
+  $("#avatar_file_field").on("change", function (e) {
+    var files = e.target.files;
+    var d = new $.Deferred().resolve();
+    $.each(files, function (i, file) {
+      d = d.then(function () {
+        return previewImage(file);
+      });
+    });
+  });
+
+  var previewImage = function (imageFile) {
+    var reader = new FileReader();
+    var img = new Image();
+    var def = $.Deferred();
+    reader.onload = function (e) {
+      $("#avatar_preview").empty();
+      $("#avatar_preview").append(img);
+      img.src = e.target.result;
+      def.resolve(img);
+    };
+    reader.readAsDataURL(imageFile);
+    return def.promise();
+  };
+});
