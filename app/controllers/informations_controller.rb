9ee5e223d9cf452cbeed2fdfe26589f7f5b88143
masao <>
2021-02-03 21:55:37 +0900

    createアクションの実装

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -9,6 +9,21 @@ class InformationsController < ApplicationController
   end
 
   def new
-    
+    @information = current_user.informations.build
+  end
+
+  def create
+    @information = current_user.informations.build(information_params)
+    if @information.save
+      redirect_to information_path(@information), notice: '保存できたよ'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+
+  private
+  def information_params
+    params.require(:information).permit(:title, :content)
   end
 end
