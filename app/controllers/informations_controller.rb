674b15f988d05d4ed6a3c8668d13ed97f7fb8955
masao <>
2021-02-04 22:34:03 +0900

    編集機能実装 editとupdateアクションを追加

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -22,6 +22,20 @@ class InformationsController < ApplicationController
     end
   end
 
+  def edit
+    @information = current_user.informations.find(params[:id])
+  end
+
+  def update
+    @information = current_user.informations.find(params[:id])
+    if @information.update(information_params)
+      redirect_to information_path(@information), notice: '更新できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
   private
   def information_params
     params.require(:information).permit(:title, :content)
