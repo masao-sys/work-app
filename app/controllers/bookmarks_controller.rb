a84ec8d51a36a9bd4ec0c96106c087ca656d0e25
masao <>
2021-02-11 18:53:19 +0900

    bookmarkをAjaxで対応

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,20 +4,22 @@ class BookmarksController < ApplicationController
   def show
     information = Information.find(params[:information_id])
     bookmark_status = current_user.has_bookmark?(information)
+    
     render json: { hasBookmark: bookmark_status }
   end
 
   def create
     information = Information.find(params[:information_id])
     information.bookmarks.create!(user_id: current_user.id)
-    redirect_to information_path(information)
+
+    render json: { status: 'ok' }
   end
 
   def destroy
     information = Information.find(params[:information_id])
     bookmark = information.bookmarks.find_by!(user_id: current_user.id)
-
     bookmark.destroy!
-    redirect_to information_path(information)
+
+    render json: { status: 'ok' }
   end
 end
--- app/javascript/packs/bookmark.js
+++ app/javascript/packs/bookmark.js
@@ -1,5 +1,16 @@
 import $ from 'jquery'
 import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+
+const handleBookmarkDisplay = (hasBookmark) => {
+  if (hasBookmark) {
+    $('.active-bookmark').removeClass('hidden')
+  } else {
+    $('.inactive-bookmark').removeClass('hidden')
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const dataset = $('#information-show').data()
@@ -7,10 +18,34 @@ document.addEventListener('DOMContentLoaded', () => {
   axios.get(`/informations/${informationId}/bookmark`)
     .then((response) => {
       const hasBookmark = response.data.hasBookmark
-      if (hasBookmark) {
-        $('.active-bookmark').removeClass('hidden')
-      } else {
-        $('.inactive-bookmark').removeClass('hidden')
-      }
+      handleBookmarkDisplay(hasBookmark)
     })
+
+  $('.inactive-bookmark').on('click', () => {
+    axios.post(`/informations/${informationId}/bookmark`)
+      .then((response) => {
+        if (response.data.status === 'ok') {
+          $('.active-bookmark').removeClass('hidden')
+          $('.inactive-bookmark').addClass('hidden')
+        }
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
+
+  $('.active-bookmark').on('click', () => {
+    axios.delete(`/informations/${informationId}/bookmark`)
+      .then((response) => {
+        if (response.data.status === 'ok') {
+          $('.active-bookmark').addClass('hidden')
+          $('.inactive-bookmark').removeClass('hidden')
+        }
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
 })
