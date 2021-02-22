7e92ba4eb36d3661acc1fcd89138bc6ca66a9548
masao <>
2021-02-22 13:14:15 +0900

    indexページの作成

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -3,6 +3,7 @@ class ManagementsController < ApplicationController
   before_action :if_not_management, only: [:new, :create, :edit, :update, :destroy]
 
   def index
+    @managements = Management.all.order(created_at: :desc)
   end
 
   private
--- app/models/management.rb
+++ app/models/management.rb
@@ -15,4 +15,12 @@
 #
 class Management < ApplicationRecord
   belongs_to :user
+
+  def author_name
+    user.display_name
+  end
+
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
+  end
 end
