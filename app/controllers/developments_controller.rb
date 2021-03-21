c23b17de8c1e2970b8e2ff0eea6c259070baa16b
masao <>
2021-03-21 21:36:19 +0900

    show、createページの作成

--- app/controllers/developments_controller.rb
+++ app/controllers/developments_controller.rb
@@ -6,6 +6,29 @@ class DevelopmentsController < ApplicationController
     @developments = Development.all.order(created_at: :desc)
   end
 
+  def show
+    @development = Development.find(params[:id])
+  end
+
+  def new
+    @development = current_user.developments.build
+  end
+
+  def create
+    @development = current_user.developments.build(development_params)
+    if @development.save
+      redirect_to development_path(@development), notice: '保存できたました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+
+  private
+  def development_params
+    params.require(:development).permit(:title, :content, images: [])
+  end
+
   private
   def if_not_development
     redirect_to developments_path unless current_user.department == 'development'
