1b4fbb0825ec6df818ca79150c5bbf6c52d272c0
masao <>
2021-03-24 13:24:13 +0900

    写真が４枚添付された時はエラー発生するテスト

--- spec/models/information_spec.rb
+++ spec/models/information_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe Information, type: :model do
     end
   end
 
+  context "写真を4枚添付したとき" do
+    let!(:information_four_attach) { build(:information_four_attach, user: user) }
+
+    before do
+      information_four_attach.save
+    end
+
+    it '写真を保存できない' do
+      expect(information_four_attach.errors.messages[:images][0]).to eq('は3つまで指定できます')
+    end
+  end
+
   context 'タイトルの入力がない時' do
     let!(:information) { build(:information, title: Faker::Lorem.characters(number: 0), user: user) }
 
