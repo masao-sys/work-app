f5f3e5497bbd1b28c9f22d2cb82f3360c50e9e9e
masao <>
2021-03-29 21:17:24 +0900

    sale_commentのAPIテスト実施

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,4 +2,8 @@ FactoryBot.define do
   factory :management_comment, class: ManagementComment do
     content { Faker::Lorem.characters(number: 300) }
   end
+
+  factory :sale_comment, class: SaleComment do
+    content { Faker::Lorem.characters(number: 300) }
+  end
 end
