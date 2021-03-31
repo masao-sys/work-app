95a1150e15b15c6b2fd171fa7816aad33151431e
masao <>
2021-03-31 12:52:47 +0900

    詳細リンクをクリックして詳細ページの表示テスト実施

--- spec/system/information_spec.rb
+++ spec/system/information_spec.rb
@@ -8,10 +8,20 @@ RSpec.describe 'Information', type: :system do
     sign_in admin
   end
 
-  it '記事一覧が表示される' do
+  it '全体連絡一覧が表示される' do
     visit root_path
     informations.each do |information|
-      expect(page).to have_content(information.title)
+      expect(page).to have_css('.card_title', text: information.title)
     end
+  end
+
+  it '全体連絡の詳細を表示できる' do
+    visit root_path
+
+    information = informations.last
+    page.first('.card_display_button').click
+
+    expect(page).to have_css('.information_title', text: information.title)
+    expect(page).to have_css('.information_content', text: information.content)
   end
 end
